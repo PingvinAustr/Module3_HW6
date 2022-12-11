a64aadc51efde72fd4511cf8ee2f9206e6ef3b87
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2022-12-11 16:17:26 +0200

    Implemented MessageBox class & functionality

---
+++ Module3_HW6/MessageBox.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Module3_HW6;
+public class MessageBox
+{
+    public MessageBox()
+    {
+        TryCloseWindow = WindowCloseHandler;
+    }
+
+    public delegate void WindowHandler(StateEnum.State state);
+    public event WindowHandler TryCloseWindow;
+    public StateEnum.State State { get; set; }
+    public async void Open()
+    {
+        Console.WriteLine("Window was opened");
+        System.Threading.Thread.Sleep(3000);
+        Console.WriteLine("Window was closed by a user");
+
+        Random random = new Random();
+        int randomNum = random.Next(0, 100);
+
+        Console.WriteLine("Randomly generated number " +
+            "(if <=50 State=OK, else State=Cancel) - " + randomNum);
+
+        if (randomNum <= 50)
+        {
+            TryCloseWindow.Invoke(StateEnum.State.Ok);
+        }
+        else
+        {
+            TryCloseWindow.Invoke(StateEnum.State.Cancel);
+        }
+    }
+
+    public void WindowCloseHandler(StateEnum.State state)
+    {
+        State = state;
+    }
+}
---
+++ Module3_HW6/StateEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3_HW6
+{
+    public class StateEnum
+    {
+        public enum State
+        {
+            Ok,
+            Cancel
+        }
+    }
+}
